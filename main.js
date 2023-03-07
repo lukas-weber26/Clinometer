4c805cc129e89ab96cc0cd3fa6fe459c20c9f37f
Lukas Weber <lukasweber@Lukass-MacBook-Pro-2.local>
2023-03-07 18:13:53 -0500

    tested functionality

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 console.log("Loaded")
 const h2 = document.getElementById("beta")
+const h3 = document.getElementById("alpha")
 
 
-DeviceMotionEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
+if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    DeviceMotionEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.addEventListener('devicemotion', (e) => {
+            console.log(e)
+            h2.innerText = e
+          });
+        }
+      })
+      .catch(console.error);
+  } else {
     window.addEventListener('devicemotion', (e) => {
-        console.log(e.acceleration)
-        h2.innerHTML = "Test"
-    })
+        console.log(e)
+        h2.innerText = e
+      });
   }
-})
-.catch(console.error)
+
+
+  if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+    DeviceOrientationEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.addEventListener('deviceorientation', (e) => {
+            console.log(e)
+            h3.innerText = e.alpha
+          });
+        }
+      })
+      .catch(console.error);
+  } else {
+    window.addEventListener('deviceorientation', (e) => {
+        console.log(e)
+        h3.innerText = e.alpha
+      });
+  }
+
