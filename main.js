5b5d6edfc97c9f569d542f984e516255bd9de995
Lukas Weber <lukasweber@Lukass-MacBook-Pro-2.local>
2023-03-07 18:38:58 -0500

    last attempt

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 console.log("Loaded")
 const h2 = document.getElementById("beta")
+const start = document.getElementById("start")
 
-
-if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    DeviceMotionEvent.requestPermission()
-      .then(permissionState => {
-        if (permissionState === 'granted') {
-          window.addEventListener('devicemotion', (e) => {
+start.onclick(() => {
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission()
+          .then(permissionState => {
+            if (permissionState === 'granted') {
+              window.addEventListener('devicemotion', (e) => {
+                console.log(e)
+                //h2.innerText = e
+              });
+            }
+          })
+          .catch(console.error);
+      } else {
+        window.addEventListener('devicemotion', (e) => {
             console.log(e)
             //h2.innerText = e
           });
-        }
-      })
-      .catch(console.error);
-  } else {
-    window.addEventListener('devicemotion', (e) => {
-        console.log(e)
-        //h2.innerText = e
-      });
-  }
-
-
-  if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-    DeviceOrientationEvent.requestPermission()
-      .then(permissionState => {
-        if (permissionState === 'granted') {
-          window.addEventListener('deviceorientation', (e) => {
+      }
+    
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+        DeviceOrientationEvent.requestPermission()
+        .then(permissionState => {
+            if (permissionState === 'granted') {
+            window.addEventListener('deviceorientation', (e) => {
+                console.log(e)
+                h3.innerText = e.beta
+            });
+            }
+        })
+        .catch(console.error);
+    } else {
+        window.addEventListener('deviceorientation', (e) => {
             console.log(e)
             h3.innerText = e.beta
-          });
-        }
-      })
-      .catch(console.error);
-  } else {
-    window.addEventListener('deviceorientation', (e) => {
-        console.log(e)
-        h3.innerText = e.beta
-      });
-  }
+        });
+    }
+})
+
+
+
 
