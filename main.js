95249a088ce78b44ae7170e54ba6e0f1abe00e16
Lukas Weber <lukasweber@Lukass-MacBook-Pro-2.local>
2023-03-07 17:41:36 -0500

    Added functionality

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 console.log("Loaded")
+const h2 = Document.getElementById("beta")
 
 window.addEventListener("devicemotion", handleMotion, true);
 
 function handleMotion(event) {
     console.log(event.beta)
+    h2.innerHTML = event.beta
 }
+
