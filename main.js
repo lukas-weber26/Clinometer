18412bbaf169bb40101d92c8a886b71ab020bd87
Lukas Weber <lukasweber@Lukass-MacBook-Pro-2.local>
2023-03-07 19:20:22 -0500

    orientationCheck

--- main.js
+++ main.js
@@ -1,45 +1,9 @@
 console.log("Loaded")
-const h2 = document.getElementById("beta")
-const start = document.getElementById("start")
+const angle = document.getElementById("angle")
 
-start.onclick(() => {
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-        DeviceMotionEvent.requestPermission()
-          .then(permissionState => {
-            if (permissionState === 'granted') {
-              window.addEventListener('devicemotion', (e) => {
-                console.log(e)
-                //h2.innerText = e
-              });
-            }
-          })
-          .catch(console.error);
-      } else {
-        window.addEventListener('devicemotion', (e) => {
-            console.log(e)
-            //h2.innerText = e
-          });
-      }
-    
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-        DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-            if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', (e) => {
-                console.log(e)
-                h3.innerText = e.beta
-            });
-            }
-        })
-        .catch(console.error);
-    } else {
-        window.addEventListener('deviceorientation', (e) => {
-            console.log(e)
-            h3.innerText = e.beta
-        });
-    }
-})
+window.addEventListener("deviceorientation", handleOrientation, true);
 
-
-
-
+function handleOrientation(event) {
+    const beta = event.beta;
+    angle.textContent = beta;
+  }
